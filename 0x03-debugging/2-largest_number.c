4a8a9e818198e0f95ac91e8e8670d367ba2caa43
Adeniyi Ayobami <ayobamisamuel58@gmail.com>
2023-02-24 06:07:49 -0800

    2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -11,7 +11,6 @@
 int largest_number(int a, int b, int c)
 
 {
-
 	int largest;
 
 	if (a >= b && a >= c)
@@ -27,5 +26,4 @@ int largest_number(int a, int b, int c)
 	largest = c;
 	}
 	return (largest);
-
 }
