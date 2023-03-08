8dee343cfbce4be238dfc9585485a974d0ec2d48
Adeniyi Ayobami <ayobamisamuel58@gmail.com>
2023-03-08 04:08:49 -0800

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -7,13 +7,13 @@ int _sqrt(int n, int i);
  * _sqrt_recursion - Returns the natural square root of a number
  * @n: number to calculate the natural square root
  *
- * Return: the natual square root
+ * Return: the natural square root
  */
 
 int _sqrt_recursion(int n)
-{       
-        return (_sqrt(n, 1));
-}       
+{
+	return (_sqrt(n, 1));
+}
 
 /**
  * _sqrt - Calculates natural square root
